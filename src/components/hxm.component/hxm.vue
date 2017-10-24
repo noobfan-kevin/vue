ee15d9cd59686ef41aafa168da1052a16247bfa8
526371075@qq.com <q2670187>
2017-10-24 18:00:41 +0800

    new file test still has issue

---
+++ src/components/hxm.component/hxm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hxm-container" v-if="show">
+      <ul>
+          <li v-for="item in items" :key="item.name">
+              <h3>{{item.name}}</h3>
+              <br>
+              <span>he {{item.hasPhone?'has':'don\'t has'}} phone</span>
+              <br>
+              <span>his age is {{item.age}}</span>
+          </li>
+      </ul>
+      <button type="checkbox" :aria-checked="show" @change="val => show = val"></button>
+  </div>
+</template>
+
+// <script>
+// import hxmData from './hxm'
+//     export default {
+//   name: 'hxm',
+//   data () {return hxmData}
+//   }
+
+// </script>
+
+<script>
+    import hxmData from './hxm'
+    export default new Vue({
+        el: 'hxm',
+        data: hxmData,
+        showAlter: function () {
+            window.alert('here is the show alter function demo.')
+        },
+        render: h => (this)
+    })
+    .$watch('show', function (_new, _old) {
+    window.alter('you toggle all info, before is ' + _old + ' now it change to ' + _new)
+    })
+    .mount('hxm');
+</script>
